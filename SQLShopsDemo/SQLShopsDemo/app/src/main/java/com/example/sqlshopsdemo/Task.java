c072afbcf9059ecc0693348091cd26ce3c0a540a
Errlin Deleon <EDeleon@student.neumont.edu>
2018-08-09 11:41:13 -0600

    Updated the Tasks class

--- SQLShopsDemo/SQLShopsDemo/app/src/main/java/com/example/sqlshopsdemo/Task.java
+++ SQLShopsDemo/SQLShopsDemo/app/src/main/java/com/example/sqlshopsdemo/Task.java
@@ -1,5 +1,10 @@
 package com.example.sqlshopsdemo;
 
+import android.content.Intent;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
 import java.sql.Time;
 import java.util.Timer;
 
@@ -7,20 +12,33 @@ public class Task {
     private int id;
     private String task;
     private String time;
+    private Chronometer chronometer;
+    private long pauseOffSet = 0;
+    private boolean isRunning = false;
+
     public Task()
     {
     }
+
+    public Task(View v, int id)
+    {
+        chronometer = (Chronometer)v.findViewById(id);;
+    }
+
+
     public Task(int id, String task, String time)
     {
         this.id = id;
         this.task = task;
         this.time = time;
     }
+
     public Task(String task, String time)
     {
         this.task = task;
         this.time = time;
     }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -40,4 +58,35 @@ public class Task {
     public String getTask() {
         return task;
     }
+
+    public Chronometer getChronometer() {
+        return chronometer;
+    }
+    public void setChronometer(Chronometer chronometer) {
+        this.chronometer = chronometer;
+    }
+    public long getPauseOffSet() {
+        return pauseOffSet;
+    }
+    public void setPauseOffSet(long pauseOffSet) {
+        this.pauseOffSet = pauseOffSet;
+    }
+
+
+    public void Start() {
+        if(!isRunning){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffSet - Long.parseLong(time));
+            chronometer.start();
+            isRunning = true;
+        }
+    }
+    public void Stop() {
+        if(isRunning){
+            chronometer.stop();
+            pauseOffSet = SystemClock.elapsedRealtime() - chronometer.getBase() - Long.parseLong(time);
+            isRunning = false;
+            time = chronometer.getText().toString();
+        }
+    }
+
 }
